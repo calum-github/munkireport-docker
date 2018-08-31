0eb672c05260ca7e547446abaffb8b2a817244ef
Calum Hunter <calum.hunter@outware.com.au>
2018-08-31 17:19:03 +1000

    dont create a config.php file with out start script

--- start.sh
+++ start.sh
@@ -2,17 +2,8 @@
 
 # Use the environmental variables passed to the Docker container and use them to:
 
-# Configure the Database Connection
-echo "- Copying config_default.php to /www/munkireport/config.php"
-cp /www/munkireport/config_default.php /www/munkireport/config.php
-
-
-# Fire up PHP and then start Nginx in non daemon mode so docker has something to keep running
-echo ""
-echo "*** Configuration of the Munki Report php file complete ***"
-echo ""
-echo "*** Starting php7-fpm ***"
+echo "- Starting php7-fpm ***"
 service php7.2-fpm start
 echo ""
-echo "*** Starting NginX ***"
+echo "- Starting NginX ***"
 nginx
